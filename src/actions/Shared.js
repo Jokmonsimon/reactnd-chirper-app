45c45ab13cc5bdf775edddc7ced64fe7185b176b
Ojok Simon Peter <simonpeter.oj@gmail.com>
2021-11-03 14:13:35 +0300

    actionCreators

--- src/actions/Shared.js
+++ src/actions/Shared.js
@@ -1,6 +1,7 @@
 import { getInitialData } from "../utils/api";
 import { receiveUsers } from "./Users";
 import { receiveTweets } from "./Tweets";
+import { setAuthedUser } from "./AuthedUser";
 
 const AUTHED_ID = "tylermcginnis";
 
@@ -9,6 +10,7 @@ export function handleInitialData() {
     return getInitialData().then(({ users, tweets }) => {
       dispatch(receiveUsers(users));
       dispatch(receiveTweets(tweets));
+      dispatch(setAuthedUser(AUTHED_ID));
     });
   };
 }

6ff136470c65523c1a3d87ddc7d3aab526414509
Ojok Simon Peter <simonpeter.oj@gmail.com>
2021-11-03 18:06:05 +0300

    React-redux - twitter clone

--- src/components/Tweets.js
+++ src/components/Tweets.js
@@ -12,7 +12,7 @@ class Tweet extends Component {
   };
   toParent = (e, id) => {
     e.preventDefault();
-    // todo: Redirect to parent Tweet
+    // todo: Redirect to Parent Tweet
   };
   render() {
     const { tweet } = this.props;
